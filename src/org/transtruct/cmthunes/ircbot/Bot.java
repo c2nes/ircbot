646ff21135bd768f664885fb13f2a26b6557ad12
rob <rob@katahdin>
2012-03-18 22:28:12 -0400

    Actually fix order

--- src/org/transtruct/cmthunes/ircbot/Bot.java
+++ src/org/transtruct/cmthunes/ircbot/Bot.java
@@ -29,7 +29,7 @@ public class Bot {
         CalcApplet calcApplet = new CalcApplet();
         WeatherApplet weatherApplet = new WeatherApplet();
         StatsApplet statsApplet = new StatsApplet(logger);
-        BashApplet bashApplet = new BashApplet();
+        
         botChannelListener.registerApplet("bash", bashApplet);
 
         botChannelListener.registerApplet("gh", groupHugApplet);
