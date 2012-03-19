4fa88bb6009d337b78073091c35cb9b20eb8c820
Chris Thunes <cmthunes@ncsu.edu>
2012-03-19 00:27:51 -0400

    Cache more results from Bash

--- src/org/transtruct/cmthunes/ircbot/applets/BashApplet.java
+++ src/org/transtruct/cmthunes/ircbot/applets/BashApplet.java
@@ -2,39 +2,93 @@ package org.transtruct.cmthunes.ircbot.applets;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
 
-import org.jsoup.Jsoup;
-import org.jsoup.helper.Validate;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jsoup.*;
+import org.jsoup.nodes.*;
 
-import org.transtruct.cmthunes.util.*;
 import org.transtruct.cmthunes.irc.*;
 
-
 public class BashApplet implements BotApplet {
+    private Thread quoteFetcher;
+    private ArrayBlockingQueue<String[]> quotes;
 
-    public String[] getQuote() {
-        Document doc = null;
+    private class GetQuotes implements Runnable {
+        @Override
+        public void run() {
+            while(true) {
+                BashApplet.this.populateQuotes();
+            }
+        }
+    }
+
+    public BashApplet() {
+        this.quotes = new ArrayBlockingQueue<String[]>(10);
+
+        this.quoteFetcher = new Thread(new GetQuotes());
+        this.quoteFetcher.setDaemon(true);
+        this.quoteFetcher.start();
+    }
+
+    private void populateQuotes() {
+        Document doc;
+        String quoteId = null;
+        String quoteScore = null;
+
         try {
-            doc = Jsoup.connect("http://bash.org/?random").get();
+            doc = Jsoup.connect("http://bash.org/?random1").get();
         } catch (IOException e) {
+            e.printStackTrace();
+            return;
         }
-        Element p = doc.select("p.qt").first();
-        String html = p.html();
-        html = html.replaceAll("&lt;", "<");
-        html = html.replaceAll("&gt;", ">");
-        String[] text = html.split("<br />");
-        for (int i=0; i < text.length; i++) {
-            text[i] = text[i].trim();
+
+        for(Element element : doc.select("p.quote, p.qt")) {
+            if(element.className().equals("quote")) {
+                quoteId = element.select("b").first().text();
+                quoteScore = element.ownText();
+            } else {
+                StringBuilder buffer = new StringBuilder();
+                ArrayList<String> quote = new ArrayList<String>();
+                String[] lines;
+
+                for(Node node : element.childNodes()) {
+                    if(node instanceof TextNode) {
+                        buffer.append(((TextNode) node).text().trim());
+                    } else if(node instanceof Element) {
+                        if(((Element) node).tagName().equals("br")) {
+                            buffer.append("\n");
+                        }
+                    }
+                }
+
+                lines = buffer.toString().split("\n");
+
+                if(lines.length == 1) {
+                    quote.add(String.format("%s %s: %s", quoteId, quoteScore, lines[0]));
+                } else {
+                    quote.add(String.format("%s %s:", quoteId, quoteScore));
+
+                    for(String line : lines) {
+                        quote.add(" " + line);
+                    }
+                }
+
+                try {
+                    this.quotes.put(quote.toArray(new String[0]));
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        return text;
     }
 
     public void run(IRCChannel channel, IRCUser from, String command, String[] args, String unparsed) {
-        String[] quote = this.getQuote();
-        channel.writeMultiple(quote);
+        try {
+            String[] quote = this.quotes.take();
+            channel.writeMultiple(quote);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-
 }
-
