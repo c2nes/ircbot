488f9dc83330df0dce7a97c4ae53ad54459a4424
Chris Thunes <cmthunes@ncsu.edu>
2012-04-20 12:14:22 -0400

    Switch to use jsoup

--- src/org/transtruct/cmthunes/ircbot/applets/GroupHugApplet.java
+++ src/org/transtruct/cmthunes/ircbot/applets/GroupHugApplet.java
@@ -1,18 +1,19 @@
 package org.transtruct.cmthunes.ircbot.applets;
 
 import java.io.*;
-import java.net.*;
+import java.net.SocketTimeoutException;
 import java.util.*;
 
-import org.htmlparser.*;
-import org.htmlparser.lexer.*;
-import org.htmlparser.util.*;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.transtruct.cmthunes.util.*;
 
 import org.transtruct.cmthunes.irc.*;
 
 public class GroupHugApplet implements BotApplet {
-    private URL url;
+    private String url;
     private FixedBlockingBuffer<String> confessions;
     private String errorMessage;
     private Flag error;
@@ -23,13 +24,9 @@ public class GroupHugApplet implements BotApplet {
         @Override
         public void run() {
             while(true) {
-                try {
-                    int page = page_numbers.remove(0);
-                    page_numbers.add(page);
-                    url = new URL(String.format("http://archive.grouphug.us/frontpage?page=%d", page));
-                } catch(MalformedURLException e) {
-                    e.printStackTrace();
-                }
+                int page = page_numbers.remove(0);
+                page_numbers.add(page);
+                url = String.format("http://archive.grouphug.us/frontpage?page=%d", page);
 
                 /* Wait for error flag to be cleared */
                 GroupHugApplet.this.error.waitUninterruptiblyFor(false);
@@ -56,95 +53,43 @@ public class GroupHugApplet implements BotApplet {
         this.confessionFetcher.setDaemon(true);
         this.confessionFetcher.start();
     }
-
+    
     private void populateConfessions() {
-        HttpURLConnection connection;
-        Lexer lexer;
+        Connection connection;
+        Connection.Response response;
+        Document doc;
+        String confessionId;
+        String confession;
+        String blurb;
 
         try {
-            connection = (HttpURLConnection) this.url.openConnection();
-            lexer = new Lexer(connection);
-        } catch(IOException e) {
-            this.errorMessage = "Could not establish a connection to grouphug.us";
-            this.error.set();
-            return;
+            connection = Jsoup.connect(this.url);
+            connection.userAgent("Wget/1.13.4 (linux-gnu)");
+            connection.ignoreHttpErrors(true);
+            connection.execute();
+            
+            response = connection.response();
+            if(response.statusCode() != 200) {
+                this.errorMessage = response.statusCode() + ": " + response.statusMessage();
+                this.error.set();
+                return;
+            }
 
-        } catch(ParserException e) {
-            this.errorMessage = "Parser error";
-            this.error.set();
+            doc = response.parse();
+        } catch (IOException e) {
+            e.printStackTrace();
             return;
-
         }
 
-        Node node = null;
-        int state = 0;
-        String confessionId = "";
-        while(true) {
+        for(Element element : doc.select("div.node")) {
             try {
-                node = lexer.nextNode();
-                if(node == null) {
-                    break;
-                }
-
-                if(node instanceof Tag) {
-                    Tag tag = (Tag) node;
-                    String tagId = tag.getAttribute("id");
-                    String tagName = tag.getRawTagName();
-                    String tagClass = tag.getAttribute("class");
-
-                    switch(state) {
-                    case 0:
-                        if(tagId != null && tagId.startsWith("node-")) {
-                            state = 1;
-                        }
-                        break;
-
-                    case 1:
-                        if(tagName != null && tagName.equals("a")) {
-                            node = lexer.nextNode();
-                            if(node != null && node instanceof Text) {
-                                confessionId = ((Text) node).getText();
-                            }
-                        } else if((tagName != null && tagClass != null)
-                                && (tagName.equals("div") && tagClass.contains("content"))) {
-                            StringBuffer confessionBuffer = new StringBuffer();
-                            while(true) {
-                                node = lexer.nextNode();
-                                if(node == null) {
-                                    break;
-                                }
-
-                                if(node instanceof Tag) {
-                                    tagName = ((Tag) node).getRawTagName();
-                                    if(tagName.equals("/div")) {
-                                        break;
-                                    }
-
-                                    if(tagName.equals("/p")) {
-                                        confessionBuffer.append("\n");
-                                    }
-                                }
-
-                                if(node instanceof Text) {
-                                    String confessionPart = ((Text) node).getText();
-                                    confessionPart = Translate.decode(confessionPart);
-                                    confessionPart = confessionPart.replaceAll("\u2019", "'");
-                                    confessionBuffer.append(confessionPart.trim());
-                                }
-                            }
-
-                            if(confessionBuffer.length() > 0) {
-                                String confession = String.format("%s: %s", confessionId,
-                                        confessionBuffer.toString());
-                                this.confessions.add(confession.trim());
-                            }
-                            state = 0;
-                        }
-                        break;
-                    }
-                }
-
-            } catch(ParserException e) {
+                confessionId = element.select("a").get(0).text().trim();
+                confession = element.select("p").get(0).text().trim();
+                blurb = confessionId + ": " + confession;
+                
+                this.confessions.add(blurb);
+            } catch(IndexOutOfBoundsException e) {
+                /* Failed to find needed elements */
                 this.errorMessage = "Parser error";
                 this.error.set();
                 return;
