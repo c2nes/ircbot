75b723849fe0bf5f3f4417d139211a492e89a493
Chris Thunes <cmthunes@ncsu.edu>
2012-04-24 23:21:54 -0400

    Handle images being returned as results

--- src/org/transtruct/cmthunes/ircbot/applets/WolframAlphaApplet.java
+++ src/org/transtruct/cmthunes/ircbot/applets/WolframAlphaApplet.java
@@ -30,6 +30,15 @@ public class WolframAlphaApplet implements BotApplet {
     public void run(IRCChannel channel, IRCUser from, String command, String[] args, String unparsed) {
         URLBuilder url;
         String query = unparsed;
+        boolean foundResult = false;
+        
+        /* Search for a valid result at the following paths in this order */
+        String[] resultSearchPaths = {
+                "//pod[@primary]/subpod/plaintext",
+                "//pod[2]/subpod/plaintext",
+                "//pod[2]/subpod/img/@src"
+        };
+        
 
         if(query.trim().length() == 0) {
             channel.write("Missing query");
@@ -44,7 +53,6 @@ public class WolframAlphaApplet implements BotApplet {
         }
 
         url.setParameter("appid", this.appid);
-        url.setParameter("format", "plaintext");
         url.setParameter("reinterpret", "true");
         url.setParameter("input", query);
             
@@ -53,16 +61,18 @@ public class WolframAlphaApplet implements BotApplet {
             String success = this.xpath.evaluate("/queryresult/@success", doc);
 
             if(success.equals("true")) {
-                String result = this.xpath.evaluate("//pod[@primary]/subpod/plaintext", doc);
+                for(String path : resultSearchPaths) {
+                    String result = this.xpath.evaluate(path, doc);
                 
-                if(result.equals("")) {
-                    result = this.xpath.evaluate("//pod[2]/subpod/plaintext", doc);
+                    if(!result.equals("")) {
+                        channel.writeMultiple(result.split("\n"));
+                        foundResult = true;
+                        break;
+                    }
                 }
-                
-                if(result.equals("")) {
+
+                if(foundResult == false) {
                     channel.write("Could not get results");
-                } else {
-                    channel.writeMultiple(result.split("\n"));
                 }
             } else {
                 channel.write("Could not interpret query");
