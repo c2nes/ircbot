f1cf25bce5fed2707a8edafb6fcf9205bf0c36ca
Chris Thunes <cmthunes@ncsu.edu>
2012-05-17 14:59:14 -0400

    Generic applet for retreiving http resources

---
+++ src/org/transtruct/cmthunes/ircbot/applets/HttpGetApplet.java
@@ -0,0 +1,35 @@
+package org.transtruct.cmthunes.ircbot.applets;
+
+import java.io.IOException;
+import org.jsoup.*;
+
+import org.transtruct.cmthunes.irc.*;
+
+public class HttpGetApplet implements BotApplet {
+    private String url;
+
+    public HttpGetApplet(String url) {
+        this.url = url;
+    }
+
+    public void run(IRCChannel channel, IRCUser from, String command, String[] args, String unparsed) {
+        try {
+            Connection con;
+
+            con = Jsoup.connect(url.toString());
+            con.followRedirects(true);
+            con.execute();
+
+            if(con.response().statusCode() == 200) {
+                String[] response = BotAppletUtil.blockFormat(con.response().body(), 300, 10);
+                channel.writeMultiple(response);
+            } else {
+                channel.write("Error retreiving page: " + con.response().statusCode());
+            }
+        } catch (IOException e) {
+            channel.write("Unknown error occured");
+            e.printStackTrace();
+            return;
+        }
+    }
+}
